919bd222b31c89f40d0b1323b6e793684be7428b
yamkelajojo <mhlengijojo7@gmail.com>
2023-06-10 17:07:48 +0200

    Initial setup

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="body">
+    <h1>Dzowdzow
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+    app: {
+    head: {
+      title: "UnoTheAcivist Landing Page",
+      meta: [{ name: "UnoTheAcivist Landing Page", content: "Discography" }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+        },
+      ],
+    },
+  },
+})
