117d23b285b2af43b9df9ae6913048d7579fbb83
yamkelajojo <mhlengijojo7@gmail.com>
2023-06-12 03:48:02 +0200

    Added custom sounds and backgrounds for custom drumkit. Centered background to center when window size adjusts

--- app.vue
+++ app.vue
@@ -7,9 +7,10 @@
 <style scoped>
 .body {
   height: 100vh;
-  background-image: url("assets/images/background.jpg");
+  background-image: url("assets/images/backgrounds/1000_F_142901681_cFeNkqQjIFnjFijHufvz9PKuW6siDzXf.jpg");
   background-size: cover;
   background-repeat: no-repeat;
   background-attachment: fixed;
+  background-position: center;
 }
 </style>
